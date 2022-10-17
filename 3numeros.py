5987d0122dbe0bd32a3e7f13b294661cce1385e3
NicoJC <nejuez@misena.edu.co>
2022-10-17 18:26:55 -0500

    Funcion if

--- 3numeros.py
+++ 3numeros.py
@@ -2,3 +2,11 @@ numero1 = int(input('Ingrese el primer numero:' ))
 numero2 = int(input('Ingrese el segundo numero:' ))
 numero3 = int(input('Ingrese el tercer numero:' ))
 
+mitad = numero1 
+
+if numero2 > mitad:
+    mitad = numero2
+elif numero3 > mitad:
+    mitad = numero3
+else:
+    numero1 = mitad
