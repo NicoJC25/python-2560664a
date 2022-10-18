e5c26ef378343c7d15495dc02bae8c1a23237b9f
NicoJC <nejuez@misena.edu.co>
2022-10-18 16:01:53 -0500

    version3.6

---
+++ FuncionCua.py
@@ -0,0 +1,9 @@
+from ast import Num
+
+
+A = int(input('Ingrese el valor de A: '))
+X = int(input('Ingrese el valor de X: '))
+if A == Num:
+    P = input('¿Desea poner el valor de B?: ')
+    if P == 'si':
+        B = int(input('Ingrese el valor de B'))
