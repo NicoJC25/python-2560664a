c3416f275e32ed73451f6afcadd7949b6be8b1b6
NicoJC <nejuez@misena.edu.co>
2022-10-17 21:54:59 -0500

    version 12

--- Condicionales/SINo.py
+++ Condicionales/SINo.py
@@ -7,7 +7,7 @@ Colon = input('¿Colon descrubrio America?: ')
 Colombia = input('¿La independencia de Colombia fue en el año 1810?: ')
 Doors = input('¿The Doors fue un grupo de rock Americano?: ')
 
-
+while True:
 if Colon != Si:
     break
 elif Colombia != Si:
