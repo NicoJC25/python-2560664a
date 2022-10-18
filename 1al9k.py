1ed48662e1f03fce20cc6d1deb5f5aec9c63d50d
NicoJC <nejuez@misena.edu.co>
2022-10-17 20:45:13 -0500

    version 4

--- 1al9k.py
+++ 1al9k.py
@@ -1,12 +1,12 @@
 numero = int(input('Ponga un numero:' ))
 
-if numero <=0 and numero >=9:
+if numero >=0 and numero <=9:
     print ("El numero tiene 1 cifra")
-elif numero <=10 and numero >=99:
+elif numero >=10 and numero <=99:
     print ("El numero tiene 2 cifras")
-elif numero <=100 and numero >=999:
+elif numero >=100 and numero <=999:
     print ('El numero tiene 3 cifras')
-elif numero <=1000 and numero >=9999:
+elif numero >=1000 and numero <=9999:
     print ('El numero tiene 4 cifras')
 else:
     print ('El numero no es valido')
