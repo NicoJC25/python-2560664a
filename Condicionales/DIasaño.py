b5f9b941e48a899ce4bb06fde9687b83c1ffc0d3
NicoJC <nejuez@misena.edu.co>
2022-10-18 14:45:31 -0500

    version2.8

--- Condicionales/DIasaño.py
+++ Condicionales/DIasaño.py
@@ -1,2 +1,18 @@
-dia = int(input('Ingrese el numero del año'))
+dia = int(input('Ingrese el dia del año'))
 
+if dia >=0 and dia <=31:
+    print ("El mes es Enero")
+elif dia >=32 and dia <=60:
+    print ("El mes es Febrero")
+elif dia >=61 and dia <=92:
+    print ("El mes es Marzo")
+elif dia >=93 and dia <=123:
+    print ("El mes es Abril")
+elif dia >=124 and dia <=155:
+    print ("El mes es Mayo")
+elif dia >=156 and dia <=186:
+    print ("El mes es Junio")
+elif dia >=187 and dia <=218:
+    print ("El mes es Julio")
+else:
+    print ('El numero no es valido')
