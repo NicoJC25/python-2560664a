0aa589308db15c59e39dbac2efc73de3b435f8e8
NicoJC <nejuez@misena.edu.co>
2022-10-18 14:12:37 -0500

    version2.3

--- DFD/Fracciones.py
+++ DFD/Fracciones.py
@@ -5,10 +5,12 @@ Num1 = int(input('Ingrese el primer numerador: '))
 Den1 = int(input('Ingrese el primer denominador: '))
 Num2 = int(input('Ingrese el segundo numerador: '))
 Den2 = int(input('Ingrese el segundo denominador: '))
-ope = input('Elija el tipo de opearcion')
-if ope == suma:
+ope = input('Elija el tipo de opearcion: ')
+if ope == 'suma':
     if Den1 == Den2:
         Num1 = Num1 + Num2
         print ('La respuesta es: ',Num1, '/' ,Den1 )
     else:
         Num1 = (Num1 * Den2) + (Num2 * Den1)
+        Den1 = Den1 * Den2
+        print ('La respuesta es: ',Num1, '/' ,Den1 )
