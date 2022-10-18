563e6de828ca66b167880331a5bb76d225946e60
NicoJC <nejuez@misena.edu.co>
2022-10-18 14:10:10 -0500

    version2.2

--- DFD/Fracciones.py
+++ DFD/Fracciones.py
@@ -1,5 +1,14 @@
+from fractions import Fraction
+
+
 Num1 = int(input('Ingrese el primer numerador: '))
 Den1 = int(input('Ingrese el primer denominador: '))
 Num2 = int(input('Ingrese el segundo numerador: '))
 Den2 = int(input('Ingrese el segundo denominador: '))
-
+ope = input('Elija el tipo de opearcion')
+if ope == suma:
+    if Den1 == Den2:
+        Num1 = Num1 + Num2
+        print ('La respuesta es: ',Num1, '/' ,Den1 )
+    else:
+        Num1 = (Num1 * Den2) + (Num2 * Den1)
