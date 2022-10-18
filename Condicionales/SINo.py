b8c8b9fe6c53dfa97f712b6cba33cb0427b096e7
NicoJC <nejuez@misena.edu.co>
2022-10-17 21:53:53 -0500

    version 11

--- Condicionales/SINo.py
+++ Condicionales/SINo.py
@@ -1,4 +1,18 @@
+from curses.ascii import SI
+
+from setuptools import sic
+
+
 Colon = input('¿Colon descrubrio America?: ')
 Colombia = input('¿La independencia de Colombia fue en el año 1810?: ')
-Doors = input('¿The Doors fue un grupo de rock Americano?')
+Doors = input('¿The Doors fue un grupo de rock Americano?: ')
 
+
+if Colon != Si:
+    break
+elif Colombia != Si:
+    break
+elif Doors != Si:
+    break
+else:
+    print ('Felicidades, respondio correctamente')
