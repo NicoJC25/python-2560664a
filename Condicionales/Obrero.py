33cae7569a55d2a51c567dc29eb52d41c887edaa
NicoJC <nejuez@misena.edu.co>
2022-10-18 15:15:06 -0500

    version3.3

--- Condicionales/Obrero.py
+++ Condicionales/Obrero.py
@@ -1,5 +1,12 @@
 horas = int(input('Ingrese la cantidad de horas: '))
 
+h2 = horas - 40
+
 if horas < 40:
-    horas * 2600
-    print = ('El salario semanal es: ')
+    horas *= 2600
+    print = ('El salario semanal es: ' ,horas)
+elif horas > 40:
+    horas = 40
+    horas *= 2600
+    h2 *= 5000
+    print ('El salario semanal es: ' ,horas + h2)
