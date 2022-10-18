efeb1148228ef1124d84cf22642355f3763c4bb5
NicoJC <nejuez@misena.edu.co>
2022-10-18 13:27:46 -0500

    version2.1

---
+++ DFD/Fracciones.py
@@ -0,0 +1,5 @@
+Num1 = int(input('Ingrese el primer numerador: '))
+Den1 = int(input('Ingrese el primer denominador: '))
+Num2 = int(input('Ingrese el segundo numerador: '))
+Den2 = int(input('Ingrese el segundo denominador: '))
+
