05a9966b6fc6a84e758de76a9ca30dbfaa797e8a
NicoJC <nejuez@misena.edu.co>
2022-10-18 16:06:48 -0500

    version3.7

--- FuncionCua.py
+++ FuncionCua.py
@@ -3,7 +3,14 @@ from ast import Num
 
 A = int(input('Ingrese el valor de A: '))
 X = int(input('Ingrese el valor de X: '))
+
 if A == Num:
     P = input('¿Desea poner el valor de B?: ')
     if P == 'si':
         B = int(input('Ingrese el valor de B'))
+        if B == Num:
+            P = input('¿Desea poner el valor de C?: ')
+            if P == 'si':
+                C = int(input('Ingrese el valor de C: '))
+                F = (A*(X**2)) + (B*X) + C
+                print ('La funcion cuadratica es: ',F)
