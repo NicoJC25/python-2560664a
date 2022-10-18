5d50c0d17e77048d34dcce5eaff41e6b68a29434
NicoJC <nejuez@misena.edu.co>
2022-10-17 20:20:54 -0500

    version 2

--- 1al9k.py
+++ 1al9k.py
@@ -4,3 +4,9 @@ for i in range (0, 10):
     print ("El numero tiene 1 cifra")
 for i in range (11, 100):
     print ("El numero tiene 2 cifras")
+for i in range (101, 1000):
+    print ('El numero tiene 3 cifras')
+for i in range (1001, 10000):
+    print ('El numero tiene 4 cifras')
+    else:
+        print ('El numero no es valido')
