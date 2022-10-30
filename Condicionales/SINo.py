67781dac94a20a64cb6007378fbb1505854b65db
NicoJC <nejuez@misena.edu.co>
2022-10-29 21:18:18 -0500

    SiNo

--- Condicionales/SINo.py
+++ Condicionales/SINo.py
@@ -1,12 +1,11 @@
-for i in range (1):
-    Colon = input('¿Colon descubrio America?: ')
-    if Colon != 'si':
-        break
-    Colombia = input('¿La independencia de Colombia fue en el año 1810?: ')
-    if Colombia != 'si':
-        break
-    Doors = input('¿The Doors fue un grupo de rock Americano?: ')
-    if  Doors != 'si':
-        break
+colon=input('¿Colon descubrió América?: ')
+
+if colon=='si':
+    print('Correcto, siguiente pregunta')
+    independencia=input('¿La independencia de Colombia fue en el año 1810?: ')
+    if independencia=='si':
+        print('Correcto, siguiente pregunta')
+        the_doors=input('¿The Doors fue un grupo de rock Americano?: ')
+        if the_doors=='si':
+            print('Felicidades, respondio correctamente')
     
-    print ('Felicidades, respondio correctamente')
