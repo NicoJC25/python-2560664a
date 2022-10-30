93be53eafb4f3eb64accb9f75f820525514eed1b
NicoJC <nejuez@misena.edu.co>
2022-10-30 15:10:31 -0500

    Dinero

---
+++ Condicionales/Dinero.py
@@ -0,0 +1,26 @@
+Dinero=int(input('Escoja una cantidad de dinero: '))
+Cincu=0
+Veinte=0
+Diez=0
+Cinco=0
+Dos=0
+Mil=0
+if Dinero>=50000:
+    Cincu=Dinero//50000
+    Dinero%=50000
+if Dinero>=20000:
+    Veinte=Dinero//20000
+    Dinero%=20000
+if Dinero>=10000:
+    Diez=Dinero//10000
+    Dinero%=10000
+if Dinero>=5000:
+    Cinco=Dinero//5000
+    Dinero%=5000
+if Dinero>=2000:
+    Dos=Dinero//2000
+    Dinero%=2000
+if Dinero>=1000:
+    Mil=Dinero//1000
+    Dinero&=1000
+print ('Se usaron',Cincu,'billetes de 50000,',Veinte,'billetes de 20000,',Diez,'billetes de 10000,',Cinco,'billetes de 5000,',Dos,'billetes de 2000 y',Mil,'billetes de 1000')
