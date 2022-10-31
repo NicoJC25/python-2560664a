b60b4b935cb170955ce79178f0deb51984ab45b2
NicoJC <nejuez@misena.edu.co>
2022-10-30 22:57:45 -0500

    Condicionalescompletos

---
+++ Miscelanea1/Condicionales/Angulos.py
@@ -0,0 +1,22 @@
+#Pedir el valor
+Angulo=int(input('Ingrese el angulo en forma de grados: '))
+Radianes=3.1416*Angulo/180 #Ecuacion a radianes
+vuelta=0
+#Definir la cantidad de vueltas
+if Angulo>=360:
+    vuelta=Angulo//360
+    Angulo%=360
+    if Angulo==1:
+        print('Se realizo',vuelta,'vuelta a la circunferencia') #Si es igual a 1, cambiar el mensaje para que tenga sentido
+    else:
+        print('Se realizaron',vuelta,'vuelta a la circunferencia')
+#Definir los rangos de las cuadrantes y la conversion ponerla
+if Angulo>=0 and Angulo<=90:
+    print('Cuadrante 1,',Radianes,'convertida a radianes')
+elif Angulo>90 and Angulo<=180:
+    print('Cuadrante 2,',Radianes,'convertida a radianes')
+elif Angulo>180 and Angulo<=270:
+    print('Cuadrante 3,',Radianes,'convertida a radianes')
+elif Angulo>270 and Angulo<=360:
+    print('Cuadrante 4,',Radianes,'convertida a radianes')
+
---
+++ Miscelanea1/Condicionales/pascua.py
@@ -0,0 +1,16 @@
+#Pedir el año
+año= int(input("Ingrese un año: "))
+#Copiar y pegar las ecuaciones del ejercicio
+A= año % 19
+B= año % 4
+C= año % 7
+D= (19*A+24) % 30
+E= (2*B+4*C+6*D+5) % 7
+N= (22+D+E)
+#Condicion cuando el mes sea abril que es cuando N sea mayor a 31
+if N>31:
+    N-=31
+    print('La pascua fue el',N,'de Abril')
+elif N<=31:
+    print('La pascua fue el',N,'de Marzo') #Marzo es menor igual a 31
+
