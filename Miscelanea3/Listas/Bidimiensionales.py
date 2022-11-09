5ff4efdb4aa203651481eb08422de790ceb78552
NicoJC <nejuez@misena.edu.co>
2022-11-08 20:02:26 -0500

    ListasComprimidas

--- Miscelanea3/Listas/Bidimiensionales.py
+++ Miscelanea3/Listas/Bidimiensionales.py
@@ -14,3 +14,23 @@ board[7][0] = ROOK
 board[7][7] = ROOK
 
 print(board)
+
+
+matrix=[[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+
+print('Mostrar todos los elementos de la matriz fila por fila')
+for row in matrix:
+    print(row)
+
+print('Mostrar todos los elementos de la matriz elemento a elemento en columna')
+for row in matrix:
+    for element in row:
+        print(element)
+
+print('Mostrar todos los elementos de una matriz en formato de matriz')
+for row in matrix:
+    for wlwment in row:
+        print(element, end=' ')
+    print()
---
+++ Miscelanea3/Listas/Listas comprension/Ejercicio3.py
@@ -0,0 +1,23 @@
+import random
+'''Llenar una lista de tamaño aleatorio entre 10 y 25 elementos. Llene la lista con números aleatorios. 
+Sume los pares y saque el promedio de los impares'''
+#Crear variables y lista
+total_pares=0
+total_impares=0
+promedio_impares=0
+rango=random.randint(10,25)#Rango aleatorio de la lista entre 10 a 25 cifras
+vector=[round(random.random()*100)for i in range(rango)]
+#Para i en el rango del rango de la lista, si el residuo de la cifra de la lista al dividirse en 2 es 0, entones es par el numero. Si no es impar
+
+par=[x for x in vector if x%2==0]#lista comprimida de pares
+for x in range(len(par)):
+    total_pares+=par[x]
+impar=[x for x in vector if x%2!=0]#Lista comprimida de impares
+for x in range(len(impar)):
+    total_impares+=impar[x]
+
+promedio_impares=(total_impares//len(impar)) #Promedio de los impares
+        
+print ('La longitud de la lista es:',rango,'y los valores de la lista son:',vector)
+print ('La suma de los pares fue:',total_pares)
+print ('El promedio de los impares es:',promedio_impares)
